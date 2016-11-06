ad19b55ec157d7989af0a8bf369aeb9c3b36bb6d
nksandhu <naazksandhu@gmail.com>
2016-11-06 00:14:46 -0700

    adding server side.py

--- Server side.py
+++ Server side.py
@@ -2,7 +2,7 @@ import socket
 from threading import *
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "192.168.140.1"
+host = '192.168.140.1'
 print host
 serversocket.bind((host, 80))
    
@@ -15,7 +15,7 @@ class client(Thread):
 
     def run(self):
         while 1:
-            print('Client sent:', self.sock.recv(1024).decode())
+            print('Client sent:', self.sock.recv(80).decode())
             self.sock.send(b'Oi you sent something to me')
 
 serversocket.listen(5)
@@ -23,3 +23,4 @@ print ('server started and listening')
 while 1:
     clientsocket, address = serversocket.accept()
     client(clientsocket, address)
+    client.run()
